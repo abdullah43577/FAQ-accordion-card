6da34ed10aea29ea7939884af3fb7d111486292a
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2022-12-29 07:27:52 +0100

    fixed a little underlying problem

--- script.js
+++ script.js
@@ -4,18 +4,14 @@ const icons = document.querySelectorAll(".icon");
 const answers = document.querySelectorAll(".answers");
 const questions = document.querySelectorAll(".questions h3");
 
-// console.log(questions);
-
-for (let i = 0; i < icons.length; i++) {
-  icons[i].addEventListener("click", () => {
-    //console.log("I'm being clicked");
-
+icons.forEach((icon, i) => {
+  icon.addEventListener("click", () => {
     //rotate icon
-    icons[i].classList.toggle("rotate-icons");
+    icon.classList.toggle("rotate-icons");
 
     //toggle answers switch
     answers[i].classList.toggle("hidden");
     //toggle colors of the the elements
     questions[i].classList.toggle("color");
   });
-}
+});
